c3c6b060369468a0d28a2af6a4018a7d0b8e7076
yanndoria <yanndoria.01@gmail.com>
2025-01-03 12:24:34 -0300

    atualização 03/01

---
+++ Projetos/Prática/js/funcoes.js
@@ -0,0 +1,18 @@
+function adicionar(produto){
+    const qtd = document.getElementById('qtd_' + produto)
+    const valor = document.getElementById('valor_' + produto)
+    const total = document.getElementById('total_' + produto)
+
+    qtd.innerHTML++
+    total.innerHTML = qtd.innerHTML * valor.innerHTML
+
+}
+
+function retirar(produto){
+    const qtd = document.getElementById('qtd_' + produto)
+    const valor = document.getElementById('valor_' + produto)
+    const total = document.getElementById('total_' + produto)    
+
+    qtd.innerHTML--
+    total.innerHTML = qtd.innerHTML * valor.innerHTML
+}
