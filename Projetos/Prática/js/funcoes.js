46da4fec612967841652700423aae6f7ffb1df39
yanndoria <yanndoria.01@gmail.com>
2025-01-07 12:19:01 -0300

    aula 07.01

--- Projetos/Prática/js/funcoes.js
+++ Projetos/Prática/js/funcoes.js
@@ -3,6 +3,9 @@ function alterarQtd(produto, acao){
     const valor = document.getElementById('valor_' + produto)
     const total = document.getElementById('total_' + produto)
 
-    acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
-    total.innerHTML = qtd.innerHTML * valor.innerHTML
-}
+    if(acao == '-' && qtd.innerHTML == 0){
+        alert('Atenção, o valor não pode ser menor que 0.')
+    } else {
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--
+        total.innerHTML = qtd.innerHTML * valor.innerHTML
+    }
